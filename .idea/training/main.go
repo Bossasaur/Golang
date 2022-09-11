001520c1babe40148936d31bd30c8600040afc2b
mk-mindera <86604528+mk-mindera@users.noreply.github.com>
2022-09-11 14:07:08 -0400

    newline

--- .idea/training/main.go
+++ .idea/training/main.go
@@ -15,6 +15,6 @@ func main() {
 	var firstname string = "Susan"
 	var lastname string = "Lockwood"
 	var space string = " "
-	message := "Hello" + space + firstname + space + lastname + space + "from" + space + place
-	fmt.Println(message)
+	message := "\n" + "Hello" + space + firstname + space + lastname + space + "from" + space + place + "\n\n"
+	fmt.Print(message)
 }
